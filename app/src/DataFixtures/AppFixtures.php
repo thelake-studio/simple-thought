71c4c068de1989af6b8ea6f111342b2ee0c91212
José Luis Lázaro Jiménez de Cisneros <thelake.softwarestudio@gmail.com>
2026-01-14 09:54:44 +0100

    feat: setup AppFixtures and create first test user

--- app/src/DataFixtures/AppFixtures.php
+++ app/src/DataFixtures/AppFixtures.php
@@ -2,16 +2,39 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
+use App\Entity\Emotion;
+use App\Entity\Activity;
+use App\Entity\Tag;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    private UserPasswordHasherInterface $hasher;
+
+    public function __construct(UserPasswordHasherInterface $hasher)
+    {
+        $this->hasher = $hasher;
+    }
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        // Creamos una instancia de la entidad User
+        $user = new User();
+        $user->setEmail('alumno@daw.com');
+        $user->setNickname('ProgramadorDAW');
+        $user->setCreatedAt(new \DateTimeImmutable()); // Fecha de creación actual
 
+        // Ciframos la contraseña '123456'
+        $password = $this->hasher->hashPassword($user, '123456');
+        $user->setPassword($password);
+
+        // Le decimos a Doctrine que "prepare" este objeto para guardarlo
+        $manager->persist($user);
+
+        // Guardamos los cambios físicamente en la base de datos
         $manager->flush();
     }
 }
