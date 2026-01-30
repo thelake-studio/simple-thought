81a77e9676a38c8a746525caf746fadd6dd9762f
José Luis Lázaro Jiménez de Cisneros <thelake.softwarestudio@gmail.com>
2026-01-30 18:22:24 +0100

    feat: clean EmotionType form

--- app/src/Form/EmotionType.php
+++ app/src/Form/EmotionType.php
@@ -3,9 +3,10 @@
 namespace App\Form;
 
 use App\Entity\Emotion;
-use App\Entity\User;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ColorType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,13 +15,21 @@ class EmotionType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('value')
-            ->add('color')
-            ->add('icon')
-            ->add('user', EntityType::class, [
-                'class' => User::class,
-                'choice_label' => 'id',
+            ->add('name', TextType::class, [
+                'label' => 'Nombre de la emoción',
+                'attr' => ['placeholder' => 'Ej: Alegría, Calma...']
+            ])
+            ->add('value', IntegerType::class, [
+                'label' => 'Valor numérico (1-10)',
+                'attr' => ['min' => 1, 'max' => 10]
+            ])
+            ->add('color', ColorType::class, [
+                'label' => 'Color representativo',
+            ])
+            ->add('icon', TextType::class, [
+                'label' => 'Icono (FontAwesome)',
+                'required' => false,
+                'attr' => ['placeholder' => 'fa-smile']
             ])
         ;
     }
