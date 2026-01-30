0c95cea0292c5ac4dcd462a0c649e492baa84c68
José Luis Lázaro Jiménez de Cisneros <thelake.softwarestudio@gmail.com>
2026-01-30 18:35:42 +0100

    feat: implement create action in emotion controller

--- app/src/Controller/EmotionController.php
+++ app/src/Controller/EmotionController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Emotion;
+use App\Form\EmotionType;
 use App\Repository\EmotionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
@@ -17,6 +20,28 @@ final class EmotionController extends AbstractController
     {
         return $this->render('emotion/index.html.twig', [
             'emotions' => $emotionRepository->findAllByUser($this->getUser()),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_emotion_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EmotionRepository $emotionRepository): Response
+    {
+        $emotion = new Emotion();
+        $form = $this->createForm(EmotionType::class, $emotion);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $emotion->setUser($this->getUser());
+            $emotionRepository->save($emotion, true);
+
+            $this->addFlash('success', 'Nueva emoción añadida.');
+
+            return $this->redirectToRoute('app_emotion_index');
+        }
+
+        return $this->render('emotion/new.html.twig', [
+            'emotion' => $emotion,
+            'form' => $form,
         ]);
     }
 }
