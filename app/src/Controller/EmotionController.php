04135bb98c244ff6ab39927134cb238228d45cb3
José Luis Lázaro Jiménez de Cisneros <thelake.softwarestudio@gmail.com>
2026-01-30 18:34:14 +0100

    feat: implement index action in emotion controller

--- app/src/Controller/EmotionController.php
+++ app/src/Controller/EmotionController.php
@@ -2,17 +2,21 @@
 
 namespace App\Controller;
 
+use App\Repository\EmotionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[Route('/emotion')]
+#[IsGranted('ROLE_USER')]
 final class EmotionController extends AbstractController
 {
-    #[Route('/emotion', name: 'app_emotion')]
-    public function index(): Response
+    #[Route('/', name: 'app_emotion_index', methods: ['GET'])]
+    public function index(EmotionRepository $emotionRepository): Response
     {
         return $this->render('emotion/index.html.twig', [
-            'controller_name' => 'EmotionController',
+            'emotions' => $emotionRepository->findAllByUser($this->getUser()),
         ]);
     }
 }
