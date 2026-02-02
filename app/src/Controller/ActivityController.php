450d6efbb8a21609f7871561113538dca1d04b42
José Luis Lázaro Jiménez de Cisneros <thelake.softwarestudio@gmail.com>
2026-02-02 12:49:22 +0100

    feat: implement create action in activity controller

--- app/src/Controller/ActivityController.php
+++ app/src/Controller/ActivityController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Activity;
+use App\Form\ActivityType;
 use App\Repository\ActivityRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
@@ -17,6 +20,28 @@ final class ActivityController extends AbstractController
     {
         return $this->render('activity/index.html.twig', [
             'activities' => $activityRepository->findAllByUser($this->getUser()),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_activity_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, ActivityRepository $activityRepository): Response
+    {
+        $activity = new Activity();
+        $form = $this->createForm(ActivityType::class, $activity);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $activity->setUser($this->getUser());
+            $activityRepository->save($activity, true);
+
+            $this->addFlash('success', 'Nueva emoción añadida.');
+
+            return $this->redirectToRoute('app_activity_index');
+        }
+
+        return $this->render('activity/new.html.twig', [
+            'activity' => $activity,
+            'form' => $form,
         ]);
     }
 }
