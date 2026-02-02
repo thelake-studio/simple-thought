179f568b546f99d5cc4de5d39794d0ff06b310cd
José Luis Lázaro Jiménez de Cisneros <thelake.softwarestudio@gmail.com>
2026-02-02 12:45:31 +0100

    feat: implement index action in activity controller

--- app/src/Controller/ActivityController.php
+++ app/src/Controller/ActivityController.php
@@ -2,17 +2,21 @@
 
 namespace App\Controller;
 
+use App\Repository\ActivityRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[Route('/activity')]
+#[IsGranted('ROLE_USER')]
 final class ActivityController extends AbstractController
 {
-    #[Route('/activity', name: 'app_activity')]
-    public function index(): Response
+    #[Route('/', name: 'app_activity_index', methods: ['GET'])]
+    public function index(ActivityRepository $activityRepository): Response
     {
         return $this->render('activity/index.html.twig', [
-            'controller_name' => 'ActivityController',
+            'activities' => $activityRepository->findAllByUser($this->getUser()),
         ]);
     }
 }
