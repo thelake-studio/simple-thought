f32c052507d7c265a8333e09bcc2b80d92a7edf2
José Luis Lázaro Jiménez de Cisneros <thelake.softwarestudio@gmail.com>
2026-02-03 16:22:19 +0100

    feat: implement index action in tag controller

--- app/src/Controller/TagController.php
+++ app/src/Controller/TagController.php
@@ -2,17 +2,21 @@
 
 namespace App\Controller;
 
+use App\Repository\TagRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[Route('/tag')]
+#[IsGranted('ROLE_USER')]
 final class TagController extends AbstractController
 {
-    #[Route('/tag', name: 'app_tag')]
-    public function index(): Response
+    #[Route('/', name: 'app_tag_index', methods: ['GET'])]
+    public function index(TagRepository $tagRepository): Response
     {
         return $this->render('tag/index.html.twig', [
-            'controller_name' => 'TagController',
+            'tags' => $tagRepository->findAllByUser($this->getUser()),
         ]);
     }
 }
