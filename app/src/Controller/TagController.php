97c6db19e2831da13b03504b684b06a2d115a575
José Luis Lázaro Jiménez de Cisneros <thelake.softwarestudio@gmail.com>
2026-02-03 16:24:34 +0100

    feat: implement create action in tag controller

--- app/src/Controller/TagController.php
+++ app/src/Controller/TagController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Tag;
+use App\Form\TagType;
 use App\Repository\TagRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
@@ -17,6 +20,28 @@ final class TagController extends AbstractController
     {
         return $this->render('tag/index.html.twig', [
             'tags' => $tagRepository->findAllByUser($this->getUser()),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_tag_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, TagRepository $tagRepository): Response
+    {
+        $tag = new Tag();
+        $form = $this->createForm(TagType::class, $tag);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $tag->setUser($this->getUser());
+            $tagRepository->save($tag, true);
+
+            $this->addFlash('success', 'Etiqueta creada correctamente.');
+
+            return $this->redirectToRoute('app_tag_index');
+        }
+
+        return $this->render('tag/new.html.twig', [
+            'tag' => $tag,
+            'form' => $form,
         ]);
     }
 }
