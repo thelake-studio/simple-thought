9e2ae85a96f295a45175844fa94e58f9df9a16d7
José Luis Lázaro Jiménez de Cisneros <thelake.softwarestudio@gmail.com>
2026-02-06 20:01:33 +0100

    feat: implement index action in entry controller

--- app/src/Controller/EntryController.php
+++ app/src/Controller/EntryController.php
@@ -2,17 +2,21 @@
 
 namespace App\Controller;
 
+use App\Repository\EntryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[Route('/entry')]
+#[IsGranted('ROLE_USER')]
 final class EntryController extends AbstractController
 {
-    #[Route('/entry', name: 'app_entry')]
-    public function index(): Response
+    #[Route('/', name: 'app_entry_index', methods: ['GET'])]
+    public function index(EntryRepository $entryRepository): Response
     {
         return $this->render('entry/index.html.twig', [
-            'controller_name' => 'EntryController',
+            'entries' => $entryRepository->findAllByUser($this->getUser()),
         ]);
     }
 }
