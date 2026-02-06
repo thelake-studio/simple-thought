fac8faf27beae77fd52b28600c90d8ab8bf25063
José Luis Lázaro Jiménez de Cisneros <thelake.softwarestudio@gmail.com>
2026-02-06 20:05:03 +0100

    feat: implement create action in entry controller

--- app/src/Controller/EntryController.php
+++ app/src/Controller/EntryController.php
@@ -2,8 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Entry;
+use App\Form\EntryType;
+use App\Repository\ActivityRepository;
+use App\Repository\EmotionRepository;
 use App\Repository\EntryRepository;
+use App\Repository\TagRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
@@ -17,6 +23,49 @@ final class EntryController extends AbstractController
     {
         return $this->render('entry/index.html.twig', [
             'entries' => $entryRepository->findAllByUser($this->getUser()),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_entry_new', methods: ['GET', 'POST'])]
+    public function new(
+        Request $request,
+        EntryRepository $entryRepository,
+        EmotionRepository $emotionRepository,
+        ActivityRepository $activityRepository,
+        TagRepository $tagRepository
+    ): Response
+    {
+        $user = $this->getUser();
+
+        $entry = new Entry();
+        $entry->setDate(new \DateTime());
+
+        $form = $this->createForm(EntryType::class, $entry, [
+            'emotions' => $emotionRepository->findAllByUser($user),
+            'activities' => $activityRepository->findAllByUser($user),
+            'tags' => $tagRepository->findAllByUser($user),
+        ]);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entry->setUser($user);
+            $entry->setCreatedAt(new \DateTimeImmutable());
+
+            if ($entry->getEmotion()) {
+                $entry->setMoodValueSnapshot($entry->getEmotion()->getValue());
+            }
+
+            $entryRepository->save($entry, true);
+
+            $this->addFlash('success', '¡Entrada guardada correctamente!');
+
+            return $this->redirectToRoute('app_entry_index');
+        }
+
+        return $this->render('entry/new.html.twig', [
+            'entry' => $entry,
+            'form' => $form,
         ]);
     }
 }
