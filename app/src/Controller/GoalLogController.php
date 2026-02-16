47f45d4fd90521cdac2a02ca92857df04a525753
José Luis Lázaro Jiménez de Cisneros <thelake.softwarestudio@gmail.com>
2026-02-16 11:35:17 +0100

    feat: implement create action in goalLog controller

--- app/src/Controller/GoalLogController.php
+++ app/src/Controller/GoalLogController.php
@@ -2,17 +2,46 @@
 
 namespace App\Controller;
 
+use App\Entity\Goal;
+use App\Entity\GoalLog;
+use App\Form\GoalLogType;
+use App\Repository\GoalLogRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[IsGranted('ROLE_USER')]
 final class GoalLogController extends AbstractController
 {
-    #[Route('/goal/log', name: 'app_goal_log')]
-    public function index(): Response
+    #[Route('/goals/{id}/log/new', name: 'app_goal_log_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, Goal $goal, GoalLogRepository $goalLogRepository): Response
     {
-        return $this->render('goal_log/index.html.twig', [
-            'controller_name' => 'GoalLogController',
+        // Seguridad: Verificar que el objetivo es del usuario
+        if ($goal->getUser() !== $this->getUser()) {
+            throw $this->createAccessDeniedException();
+        }
+
+        $goalLog = new GoalLog();
+        $goalLog->setGoal($goal);
+        $goalLog->setDate(new \DateTimeImmutable('today'));
+
+        $form = $this->createForm(GoalLogType::class, $goalLog);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $goalLogRepository->save($goalLog, true);
+
+            $this->addFlash('success', 'Registro añadido al historial.');
+
+            return $this->redirectToRoute('app_goal_show', ['id' => $goal->getId()]);
+        }
+
+        return $this->render('goal_log/new.html.twig', [
+            'goal_log' => $goalLog,
+            'goal' => $goal,
+            'form' => $form,
         ]);
     }
 }
