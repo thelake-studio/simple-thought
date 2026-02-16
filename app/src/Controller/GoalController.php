52f8c5f47d782ba2928e2cfe9d7687a21d1a5f49
José Luis Lázaro Jiménez de Cisneros <thelake.softwarestudio@gmail.com>
2026-02-16 10:56:43 +0100

    feat: implement index action in goal controller

--- app/src/Controller/GoalController.php
+++ app/src/Controller/GoalController.php
@@ -2,17 +2,40 @@
 
 namespace App\Controller;
 
+use App\Entity\Goal;
+use App\Repository\GoalRepository;
+use App\Service\GoalService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[Route('/goals')]
+#[IsGranted('ROLE_USER')]
 final class GoalController extends AbstractController
 {
-    #[Route('/goal', name: 'app_goal')]
-    public function index(): Response
+    #[Route('/', name: 'app_goal_index', methods: ['GET'])]
+    public function index(GoalRepository $goalRepository, GoalService $goalService): Response
     {
+        $goals = $goalRepository->findByUser($this->getUser());
+
+        $dashboardData = [];
+
+        foreach ($goals as $goal) {
+            if ($goal->getType() === Goal::TYPE_SUM) {
+                $stats = $goalService->getProgress($goal);
+            } else {
+                $stats = $goalService->getStreak($goal);
+            }
+
+            $dashboardData[] = [
+                'entity' => $goal,
+                'stats' => $stats
+            ];
+        }
+
         return $this->render('goal/index.html.twig', [
-            'controller_name' => 'GoalController',
+            'goals_data' => $dashboardData,
         ]);
     }
 }
