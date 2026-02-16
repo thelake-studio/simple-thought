222c3c62fb1669ac5c21edd6f587edd9531f7aa2
José Luis Lázaro Jiménez de Cisneros <thelake.softwarestudio@gmail.com>
2026-02-16 10:58:39 +0100

    feat: implement create action in goal controller

--- app/src/Controller/GoalController.php
+++ app/src/Controller/GoalController.php
@@ -3,9 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Goal;
+use App\Form\GoalType;
 use App\Repository\GoalRepository;
 use App\Service\GoalService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
@@ -36,6 +38,31 @@ final class GoalController extends AbstractController
 
         return $this->render('goal/index.html.twig', [
             'goals_data' => $dashboardData,
+        ]);
+    }
+
+    #[Route('/new', name: 'app_goal_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, GoalRepository $goalRepository): Response
+    {
+        $goal = new Goal();
+        $form = $this->createForm(GoalType::class, $goal);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $goal->setUser($this->getUser());
+            // Guardamos la fecha de creación automáticamente si no está en el constructor
+            $goal->setCreatedAt(new \DateTimeImmutable());
+
+            $goalRepository->save($goal, true);
+
+            $this->addFlash('success', '¡Nuevo objetivo definido! A por ello.');
+
+            return $this->redirectToRoute('app_goal_index');
+        }
+
+        return $this->render('goal/new.html.twig', [
+            'goal' => $goal,
+            'form' => $form,
         ]);
     }
 }
