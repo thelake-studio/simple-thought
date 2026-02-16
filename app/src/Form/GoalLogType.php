4c6a00dc0f0b820ee6ddc2f5c82a062d551b041b
José Luis Lázaro Jiménez de Cisneros <thelake.softwarestudio@gmail.com>
2026-02-16 11:24:52 +0100

    feat: clean GoalLogType form

--- app/src/Form/GoalLogType.php
+++ app/src/Form/GoalLogType.php
@@ -2,10 +2,10 @@
 
 namespace App\Form;
 
-use App\Entity\Goal;
 use App\Entity\GoalLog;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,13 +14,19 @@ class GoalLogType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('date', null, [
+            ->add('date', DateType::class, [
+                'label' => 'Fecha del registro',
                 'widget' => 'single_text',
+                'attr' => ['class' => 'form-control'],
+                'input' => 'datetime_immutable',
             ])
-            ->add('value')
-            ->add('goal', EntityType::class, [
-                'class' => Goal::class,
-                'choice_label' => 'id',
+            ->add('value', IntegerType::class, [
+                'label' => 'Cantidad / Valor',
+                'attr' => [
+                    'min' => 1,
+                    'placeholder' => 'Ej: 1 (para racha) o 5000 (para pasos)'
+                ],
+                'help' => 'Si es un hábito de racha, pon "1". Si es acumulativo, pon la cantidad exacta.',
             ])
         ;
     }
