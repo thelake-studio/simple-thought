042a45f1b9b0f0fd217d41ebfbe32e10b8d8d98f
José Luis Lázaro Jiménez de Cisneros <thelake.softwarestudio@gmail.com>
2026-02-20 18:49:13 +0100

    feat: add StatsController and base template for charts

--- app/assets/controllers/chart_controller.js
+++ app/assets/controllers/chart_controller.js
@@ -3,6 +3,29 @@ import Chart from 'chart.js/auto';
 
 export default class extends Controller {
     connect() {
-        console.log('Chart.js preparado!');
+        console.log('📈 Stimulus ha detectado el canvas. ¡Pintando gráfica!');
+
+        // "this.element" hace referencia al <canvas> donde hemos puesto el data-controller
+        new Chart(this.element, {
+            type: 'bar',
+            data: {
+                labels: ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes'],
+                datasets: [{
+                    label: 'Nivel de Energía (Prueba)',
+                    data: [6, 9, 3, 5, 8],
+                    backgroundColor: 'rgba(54, 162, 235, 0.5)',
+                    borderColor: 'rgba(54, 162, 235, 1)',
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                responsive: true,
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
     }
 }
