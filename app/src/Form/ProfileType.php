6d0e6cd720ac8a8a2c86795de58b42ca29b35f92
José Luis Lázaro Jiménez de Cisneros <thelake.softwarestudio@gmail.com>
2026-02-23 12:39:22 +0100

    feat: clean ProfileType form

--- app/src/Form/ProfileType.php
+++ app/src/Form/ProfileType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,13 +14,13 @@ class ProfileType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('roles')
-            ->add('password')
-            ->add('nickname')
-            ->add('preferences')
-            ->add('createdAt', null, [
-                'widget' => 'single_text',
+            ->add('nickname', TextType::class, [
+                'label' => 'Tu Nickname',
+                'attr' => ['class' => 'form-control']
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Correo Electrónico',
+                'attr' => ['class' => 'form-control']
             ])
         ;
     }
