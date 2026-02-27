cec38769c42ed0ad69778ba10af9915afc5756c3
José Luis Lázaro Jiménez de Cisneros <thelake.softwarestudio@gmail.com>
2026-02-27 21:16:04 +0100

    docs(ProfileType): apply PHPDoc standards and set class as final

--- app/src/Form/ProfileType.php
+++ app/src/Form/ProfileType.php
@@ -9,8 +9,19 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class ProfileType extends AbstractType
+/**
+ * Formulario para la edición del perfil de usuario.
+ * Permite actualizar la información básica de la cuenta, como el apodo (nickname) y el correo electrónico.
+ */
+final class ProfileType extends AbstractType
 {
+    /**
+     * Construye el formulario añadiendo los campos necesarios.
+     *
+     * @param FormBuilderInterface $builder El constructor del formulario.
+     * @param array $options Opciones adicionales pasadas al formulario.
+     * @return void
+     */
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
@@ -25,6 +36,13 @@ class ProfileType extends AbstractType
         ;
     }
 
+    /**
+     * Configura las opciones por defecto del formulario.
+     * Vincula este formulario directamente con la entidad User.
+     *
+     * @param OptionsResolver $resolver El resolutor de opciones de Symfony.
+     * @return void
+     */
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
